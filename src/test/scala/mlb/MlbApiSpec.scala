e1aa76aed8f1f9dc756dca9e698ff13d81e49f87
Mohammed Saber BELLAAZIRI <99510825+msaber69@users.noreply.github.com>
2023-07-14 23:01:08 +0200

    Test cases v1 - errors to be fixed -

--- src/test/scala/mlb/MlbApiSpec.scala
+++ src/test/scala/mlb/MlbApiSpec.scala
@@ -5,60 +5,97 @@ import zio.test.environment.TestEnvironment
 import mlb.MlbApi
 import mlb.Game
 
+case class UpdateResult(rowsAffected: Int)
+
 object MlbApiSpec extends DefaultRunnableSpec {
 
-  val mockApi: MlbApi.Service = new MlbApi.Service {
-    override def create: ZIO[ZConnectionPool, Throwable, Unit] =
-      ZIO.unit
+  // Helper function to create a Game object
+  def createGame(date: String, team1: String, team2: String): Game =
+    Game(
+      date = date,
+      season = 2021,
+      neutral = false,
+      playoff = false,
+      team1 = team1,
+      team2 = team2,
+      elo1_pre = 100.0,
+      elo2_pre = 200.0,
+      elo_prob1 = 0.6,
+      elo_prob2 = 0.4,
+      elo1_post = 120.0,
+      elo2_post = 180.0,
+      rating1_pre = 1.0,
+      rating2_pre = 2.0,
+      pitcher1 = "Pitcher1",
+      pitcher2 = "Pitcher2",
+      pitcher1_rgs = 6.0,
+      pitcher2_rgs = 7.0,
+      pitcher1_adj = 1.0,
+      pitcher2_adj = 1.0,
+      rating_prob1 = 0.7,
+      rating_prob2 = 0.3,
+      rating1_post = 1.5,
+      rating2_post = 2.5,
+      score1 = 5,
+      score2 = 3
+    )
 
-    override def insertRows: ZIO[ZConnectionPool, Throwable, UpdateResult] =
-      ZIO.succeed(UpdateResult(1))
+  val mlbApiSuite: Spec[TestEnvironment, TestFailure[Throwable], TestSuccess] =
+    suite("MlbApi")(
+      testM("create table") {
+        for {
+          _ <- ZIO.succeed(MlbApi.create)
+        } yield assert(())(isUnit)
+      },
+      testM("insert rows") {
+        for {
+          result <- ZIO.succeed(MlbApi.insertRows)
+        } yield assert(result)(equalTo(UpdateResult(1)))
+      },
+      testM("load all games") {
+        for {
+          games <- MlbApi.loadAllGames
+        } yield assert(games)(isEmpty)
+      },
+      testM("select game") {
+        for {
+          game <- MlbApi.selectGame("gameId")
+        } yield assert(game)(isNone)
+      },
+      test("predict outcome") {
+        val game = Game(
+          date = "2023-07-13",
+          season = 2023,
+          neutral = false,
+          playoff = false,
+          team1 = "TeamA",
+          team2 = "TeamB",
+          elo1_pre = 1500.0,
+          elo2_pre = 1500.0,
+          elo_prob1 = 0.5,
+          elo_prob2 = 0.5,
+          elo1_post = 1500.0,
+          elo2_post = 1500.0,
+          rating1_pre = 0.0,
+          rating2_pre = 0.0,
+          pitcher1 = "PitcherA",
+          pitcher2 = "PitcherB",
+          pitcher1_rgs = 0.0,
+          pitcher2_rgs = 0.0,
+          pitcher1_adj = 0.0,
+          pitcher2_adj = 0.0,
+          rating_prob1 = 0.0,
+          rating_prob2 = 0.0,
+          rating1_post = 0.0,
+          rating2_post = 0.0,
+          score1 = 0,
+          score2 = 0
+        )
+        val prediction = MlbApi.predictOutcome(game)
+        assert(prediction)(equalTo("Expected prediction"))
+      }
+    )
 
-    override def loadAllGames: ZIO[Any, Throwable, List[Game]] =
-      ZIO.succeed(List(Game("2023-07-13", 2023, neutral = false, playoff = false, "TeamA", "TeamB", 100.0, 200.0, 0.6, 0.4, 120.0, 180.0, 1.0, 2.0, "PitcherA", "PitcherB", 6.0, 7.0, 1.0, 1.0, 0.7, 0.3, 1.5, 2.5, 5, 3)))
-
-    override def selectGame(gameId: String): ZIO[ZConnectionPool, Throwable, Option[Game]] =
-      ZIO.succeed(Some(Game("2023-07-13", 2023, neutral = false, playoff = false, "TeamA", "TeamB", 100.0, 200.0, 0.6, 0.4, 120.0, 180.0, 1.0, 2.0, "PitcherA", "PitcherB", 6.0, 7.0, 1.0, 1.0, 0.7, 0.3, 1.5, 2.5, 5, 3)))
-
-    override def predictOutcome(game: Game): String =
-      if (game.team1 == "TeamA" && game.team2 == "TeamB") "TeamA"
-      else if (game.team1 == "TeamC" && game.team2 == "TeamD") "TeamD"
-      else if (game.team1 == "TeamE" && game.team2 == "TeamF") "TeamE"
-      else "Unknown"
-  }
-
-  val testApiLayer: ULayer[MlbApi] = ZLayer.succeed(mockApi)
-
-  override def spec: ZSpec[TestEnvironment, Any] = suite("MlbApi")(
-    testM("initialize should create the games table") {
-      for {
-        _ <- MlbApi.initialize.provideLayer(testApiLayer)
-        result <- MlbApi.checkIfTableExists.provideLayer(testApiLayer)
-      } yield assert(result, isTrue)
-    },
-    testM("loadAllGames should return a list of games") {
-      for {
-        games <- MlbApi.loadAllGames.provideLayer(testApiLayer)
-      } yield assert(games, hasSize(equalTo(1)))
-    },
-    testM("selectGame should return the correct game") {
-      val gameId = "2023-07-13"
-      for {
-        game <- MlbApi.selectGame(gameId).provideLayer(testApiLayer)
-      } yield assert(game, isSome(equalTo(Game("2023-07-13", 2023, neutral = false, playoff = false, "TeamA", "TeamB", 100.0, 200.0, 0.6, 0.4, 120.0, 180.0, 1.0, 2.0, "PitcherA", "PitcherB", 6.0, 7.0, 1.0, 1.0, 0.7, 0.3, 1.5, 2.5, 5, 3))))
-    },
-    testM("selectGame should return None for a non-existent game") {
-      val gameId = "2023-07-14"
-      for {
-        game <- MlbApi.selectGame(gameId).provideLayer(testApiLayer)
-      } yield assert(game, isNone)
-    },
-    test("predictOutcome should return the predicted winner") {
-      val game = Game("2023-07-13", 2023, neutral = false, playoff = false, "TeamA", "TeamB", 100.0, 200.0, 0.6, 0.4, 120.0, 180.0, 1.0, 2.0, "PitcherA", "PitcherB", 6.0, 7.0, 1.0, 1.0, 0.7, 0.3, 1.5, 2.5, 5, 3)
-      val expectedWinner = "TeamA"
-
-      val result = MlbApi.predictOutcome(game)
-      assert(result, equalTo(s"The predicted winner is: $expectedWinner"))
-    }
-  )
+  override def spec: ZSpec[TestEnvironment, TestFailure[Throwable]] =
+    mlbApiSuite
 }
